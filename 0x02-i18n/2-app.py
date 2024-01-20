5af7cd76950aca90c66d4029bd3a8a56c43d4849
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-01-20 00:55:17 +0000

    Get locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+This module sets Babel’s default locale ("en") and timezone ("UTC").
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config():
+    """
+    configure available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/', strict_slashes=False)
+def get_index() -> str:
+    """
+    return html
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
