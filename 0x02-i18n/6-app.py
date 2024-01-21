1d95c1448b0409ca4182b05cca8e69a7bb79407e
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-01-21 16:19:34 +0000

    Translations

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+This module sets Babel’s default locale ("en") and timezone ("UTC").
+"""
+from typing import Dict, Union
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config():
+    """
+    configure available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determines the appropriate locale based on priority..
+    """
+    locale = request.args.get('locale')
+    if not locale and g.user:
+        locale = g.user.get("locale")
+
+    if not local:
+        locale = request.accept_languages.best_match(app.config["LANGUAGES"])
+
+    return locale or app.config["BABEL_DEFAULT_LOCALE"]
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """
+    Validate user login details
+    Args:
+        id (str): user id
+    Returns:
+        (Dict): user dictionary if id is valid else None
+    """
+    return users.get(int(id), 0)
+
+
+@app.before_request
+def before_request():
+    """
+    Adds valid user to the global session object `g`
+    """
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+
+
+@app.route('/', strict_slashes=False)
+def get_index() -> str:
+    """
+    return html
+    """
+    return render_template('6-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
